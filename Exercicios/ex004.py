88adf3a33c0ce9da328fa4475e3d23799f68e23c
Adriany Corrêa <adrianycmc@outlook.com>
2024-11-24 13:45:06 -0300

    Update ex004.py

--- Exercicios/ex004.py
+++ Exercicios/ex004.py
@@ -8,18 +8,18 @@ print(f'Esse valor é um número? {var_algo_type.isnumeric()}')
 
 
 # Criando verificações para saber se a variável é um número, letra ou alfanumérico.
-# if var_algo_type.isnumeric() == True:
-#     print("Sim, é um número.")
-# else:
-#     print("Não é um número.")
+if var_algo_type.isnumeric() == True:
+    print("Sim, é um número.")
+else:
+    print("Não é um número.")
     
-# if var_algo_type.isalpha == True:
-#     print("Sim, é uma letra.")
-# else:
-#     print("Não é uma letra.")
+if var_algo_type.isalpha == True:
+    print("Sim, é uma letra.")
+else:
+    print("Não é uma letra.")
 
-# if var_algo_type.isalnum() == True:
-#     print("Sim, é alfanumérico.")
+if var_algo_type.isalnum() == True:
+    print("Sim, é alfanumérico.")
     
 # Encadeando as verificações com elif.    
 if var_algo_type.isnumeric():
