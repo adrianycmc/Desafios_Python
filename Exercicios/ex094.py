a6bf94c0f29b8fdf1596af9125ba7d64cea02ae5
Adriany Corrêa <adrianycmc@outlook.com>
2025-01-13 18:44:43 -0300

    Update ex094.py

--- Exercicios/ex094.py
+++ Exercicios/ex094.py
@@ -1,19 +1,29 @@
 # Desafio 094: Unindo dicionários e listas - Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas; B) A média de idade do grupo; C) Uma lista com todos os valores; D) Uma lista com todas as pessoas com idade acima da média. 
 
+pessoa = {}
 pessoas = []
 mulheres = []
 quantidade_pessoas = 0 
 
 while True:
     nome = str(input("Nome: "))
-    sexo = str(input("Sexo: ")).upper()
+    while True:
+        sexo = str(input("Sexo: ")).upper()
+        if sexo == 'M' or sexo == 'F':
+            break
+        print("ERRO! Por favor, digite apenas M ou F.")
     idade = int(input("Idade: "))
     
     pessoa = {'nome': nome, 'sexo': sexo, 'idade': idade}
     pessoas.append(pessoa)
     quantidade_pessoas += 1
     
-    continuar = str(input("Quer continuar? [S/N] ")).upper()
+    while True:
+        continuar = str(input("Quer continuar? [S/N] ")).upper()
+        if continuar == 'S' or continuar == 'N':
+            break
+        print("ERRO! Responda apenas S ou N.")
+     
     if continuar == 'N':
         break
     
@@ -27,7 +37,7 @@ if sexo == 'F':
 print("\n" + "-=" * 30 + "\n")
 
 print(f"- O grupo tem {quantidade_pessoas} pessoas.")
-print(f"- A média de idade é de {media_idade} anos.")
+print(f"- A média de idade é de {media_idade:5.2f} anos.")
 print(f"- As mulheres cadastradas foram: {', '.join(mulheres)}.")
 print("- Lista de pessoas que estão acima da média: ")
 for pessoa in pessoas:
